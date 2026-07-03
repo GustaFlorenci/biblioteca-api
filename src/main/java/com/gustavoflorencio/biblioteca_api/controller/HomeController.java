c4584fcf0fe70f044cb9c8350a1fb624970a6995
Gustavo Florenci <Gustavo.pt@icloud.com>
2026-07-03 16:36:10 +0100

    feat: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/gustavoflorencio/biblioteca_api/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.gustavoflorencio.biblioteca_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Biblioteca API funcionando!";
+    }
+
+}
